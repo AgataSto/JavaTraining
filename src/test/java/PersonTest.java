ab3c60072c01c86618d7def9a3c1867d222cd10b
Agata.Stokowy <agata.stokowy@gmail.com>
2023-12-05 14:31:51 +0100

    add new class

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -29,8 +29,8 @@ public class PersonTest {
     @Test
     void shouldCaluculateBonusforRatingGraterThan10()
     {
-        Person tomekHanks = new Person("Tom","Hanks");
-        int actual = tomekHanks.giveBonus(11,10);
+        Person tomekCruise = new Person("Tom","Hanks");
+        int actual = tomekCruise.giveBonus(11,10);
         int expected = 10;
         assertThat(actual).as("Bonus policzony nieprawidłowo").isEqualTo(expected);
     }
